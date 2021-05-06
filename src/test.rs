751187eecc4c4209b166c05f112cb01fd7f7a570
Syrone Wong <wong.syrone@gmail.com>
2021-05-06 15:53:44 +0800

    test ssl_shutdown() matrix
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- src/test.rs
+++ src/test.rs
@@ -35,7 +35,7 @@ async fn google() {
 }
 
 #[tokio::test]
-async fn server() {
+async fn server_shutdown_both_client_and_server() {
     let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
     let addr = listener.local_addr().unwrap();
 
@@ -64,6 +64,175 @@ async fn server() {
         future::poll_fn(|ctx| Pin::new(&mut stream).poll_shutdown(ctx))
             .await
             .unwrap()
+    };
+
+    let client = async {
+        let mut connector = SslConnector::builder(SslMethod::tls()).unwrap();
+        connector.set_ca_file("tests/cert.pem").unwrap();
+        let ssl = connector
+            .build()
+            .configure()
+            .unwrap()
+            .into_ssl("localhost")
+            .unwrap();
+
+        let stream = TcpStream::connect(&addr).await.unwrap();
+        let mut stream = SslStream::new(ssl, stream).unwrap();
+
+        Pin::new(&mut stream).connect().await.unwrap();
+
+        stream.write_all(b"asdf").await.unwrap();
+
+        let mut buf = vec![];
+        stream.read_to_end(&mut buf).await.unwrap();
+        assert_eq!(buf, b"jkl;");
+        future::poll_fn(|ctx| Pin::new(&mut stream).poll_shutdown(ctx))
+            .await
+            .unwrap()
+    };
+
+    future::join(server, client).await;
+}
+
+#[tokio::test]
+async fn server_shutdown_client_only() {
+    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+
+    let server = async move {
+        let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+        acceptor
+            .set_private_key_file("tests/key.pem", SslFiletype::PEM)
+            .unwrap();
+        acceptor
+            .set_certificate_chain_file("tests/cert.pem")
+            .unwrap();
+        let acceptor = acceptor.build();
+
+        let ssl = Ssl::new(acceptor.context()).unwrap();
+        let stream = listener.accept().await.unwrap().0;
+        let mut stream = SslStream::new(ssl, stream).unwrap();
+
+        Pin::new(&mut stream).accept().await.unwrap();
+
+        let mut buf = [0; 4];
+        stream.read_exact(&mut buf).await.unwrap();
+        assert_eq!(&buf, b"asdf");
+
+        stream.write_all(b"jkl;").await.unwrap();
+    };
+
+    let client = async {
+        let mut connector = SslConnector::builder(SslMethod::tls()).unwrap();
+        connector.set_ca_file("tests/cert.pem").unwrap();
+        let ssl = connector
+            .build()
+            .configure()
+            .unwrap()
+            .into_ssl("localhost")
+            .unwrap();
+
+        let stream = TcpStream::connect(&addr).await.unwrap();
+        let mut stream = SslStream::new(ssl, stream).unwrap();
+
+        Pin::new(&mut stream).connect().await.unwrap();
+
+        stream.write_all(b"asdf").await.unwrap();
+
+        let mut buf = vec![];
+        stream.read_to_end(&mut buf).await.unwrap();
+        assert_eq!(buf, b"jkl;");
+        future::poll_fn(|ctx| Pin::new(&mut stream).poll_shutdown(ctx))
+            .await
+            .unwrap()
+    };
+
+    future::join(server, client).await;
+}
+
+#[tokio::test]
+async fn server_shutdown_server_only() {
+    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+
+    let server = async move {
+        let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+        acceptor
+            .set_private_key_file("tests/key.pem", SslFiletype::PEM)
+            .unwrap();
+        acceptor
+            .set_certificate_chain_file("tests/cert.pem")
+            .unwrap();
+        let acceptor = acceptor.build();
+
+        let ssl = Ssl::new(acceptor.context()).unwrap();
+        let stream = listener.accept().await.unwrap().0;
+        let mut stream = SslStream::new(ssl, stream).unwrap();
+
+        Pin::new(&mut stream).accept().await.unwrap();
+
+        let mut buf = [0; 4];
+        stream.read_exact(&mut buf).await.unwrap();
+        assert_eq!(&buf, b"asdf");
+
+        stream.write_all(b"jkl;").await.unwrap();
+
+        future::poll_fn(|ctx| Pin::new(&mut stream).poll_shutdown(ctx))
+            .await
+            .unwrap()
+    };
+
+    let client = async {
+        let mut connector = SslConnector::builder(SslMethod::tls()).unwrap();
+        connector.set_ca_file("tests/cert.pem").unwrap();
+        let ssl = connector
+            .build()
+            .configure()
+            .unwrap()
+            .into_ssl("localhost")
+            .unwrap();
+
+        let stream = TcpStream::connect(&addr).await.unwrap();
+        let mut stream = SslStream::new(ssl, stream).unwrap();
+
+        Pin::new(&mut stream).connect().await.unwrap();
+
+        stream.write_all(b"asdf").await.unwrap();
+
+        let mut buf = vec![];
+        stream.read_to_end(&mut buf).await.unwrap();
+        assert_eq!(buf, b"jkl;");
+    };
+
+    future::join(server, client).await;
+}
+
+#[tokio::test]
+async fn server_shutdown_none() {
+    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+
+    let server = async move {
+        let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+        acceptor
+            .set_private_key_file("tests/key.pem", SslFiletype::PEM)
+            .unwrap();
+        acceptor
+            .set_certificate_chain_file("tests/cert.pem")
+            .unwrap();
+        let acceptor = acceptor.build();
+
+        let ssl = Ssl::new(acceptor.context()).unwrap();
+        let stream = listener.accept().await.unwrap().0;
+        let mut stream = SslStream::new(ssl, stream).unwrap();
+
+        Pin::new(&mut stream).accept().await.unwrap();
+
+        let mut buf = [0; 4];
+        stream.read_exact(&mut buf).await.unwrap();
+        assert_eq!(&buf, b"asdf");
+
+        stream.write_all(b"jkl;").await.unwrap();
     };
 
     let client = async {
